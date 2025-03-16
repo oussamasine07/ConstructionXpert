000c9def8513ab2824fe4410db049b9d9ef25ffe
oussamasine07 <sineoussama@gmail.com>
2025-03-16 23:45:49 +0000

    integrate theme in ui

---
+++ src/main/java/com/ConstructionXpert/controller/IndexServlet.java
@@ -0,0 +1,24 @@
+package com.ConstructionXpert.controller;
+
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+@WebServlet("/")
+public class IndexServlet extends HttpServlet {
+
+    public void init () {}
+
+    protected void doGet (HttpServletRequest req, HttpServletResponse res)
+        throws ServletException, IOException
+    {
+        RequestDispatcher rd = req.getRequestDispatcher("/views/user/dashboard.jsp");
+        rd.forward(req, res);
+    }
+
+}
