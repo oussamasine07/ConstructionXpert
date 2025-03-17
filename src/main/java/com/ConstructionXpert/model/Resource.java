e3cc4a02d0d8dd9d61278a17fd7e6c98d72ac5f1
oussamasine07 <sineoussama@gmail.com>
2025-03-17 00:25:29 +0000

    add resource model

---
+++ src/main/java/com/ConstructionXpert/model/Resource.java
@@ -0,0 +1,61 @@
+package com.ConstructionXpert.model;
+
+public class Resource {
+
+    private int resourceId;
+    private String name;
+    private int quantity;
+    private double unitPrice;
+    private double totalPrice;
+    private Supplier supplier;
+
+    public Resource () {}
+
+    public int getResourceId() {
+        return resourceId;
+    }
+
+    public void setResourceId(int resourceId) {
+        this.resourceId = resourceId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public double getUnitPrice() {
+        return unitPrice;
+    }
+
+    public void setUnitPrice(double unitPrice) {
+        this.unitPrice = unitPrice;
+    }
+
+    public double getTotalPrice() {
+        return totalPrice;
+    }
+
+    public void setTotalPrice(double totalPrice) {
+        this.totalPrice = totalPrice;
+    }
+
+    public Supplier getSupplier() {
+        return supplier;
+    }
+
+    public void setSupplier(Supplier supplier) {
+        this.supplier = supplier;
+    }
+}
