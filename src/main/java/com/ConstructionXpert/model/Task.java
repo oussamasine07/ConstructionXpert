d5c9c064608b41ae09c7f620391e3ef2e01db725
oussamasine07 <sineoussama@gmail.com>
2025-03-17 00:17:31 +0000

    make task model

---
+++ src/main/java/com/ConstructionXpert/model/Task.java
@@ -0,0 +1,53 @@
+package com.ConstructionXpert.model;
+
+public class Task {
+
+    private int taskId;
+    private String name;
+    private String description;
+    private String startDate;
+    private String endDate;
+    // add the list of resources
+
+    public Task () {}
+
+    public int getTaskId() {
+        return taskId;
+    }
+
+    public void setTaskId(int taskId) {
+        this.taskId = taskId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(String startDate) {
+        this.startDate = startDate;
+    }
+
+    public String getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(String endDate) {
+        this.endDate = endDate;
+    }
+}
