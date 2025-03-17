88817fa7bcd8cf18d24946d09d9cdddc0763ed06
oussamasine07 <sineoussama@gmail.com>
2025-03-17 00:20:58 +0000

    add supplier model

---
+++ src/main/java/com/ConstructionXpert/model/Supplier.java
@@ -0,0 +1,52 @@
+package com.ConstructionXpert.model;
+
+public class Supplier {
+
+    private int supplierId;
+    private String name;
+    private String email;
+    private String phone;
+    private String address;
+
+    public Supplier () {}
+
+    public int getSupplierId() {
+        return supplierId;
+    }
+
+    public void setSupplierId(int supplierId) {
+        this.supplierId = supplierId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+}
