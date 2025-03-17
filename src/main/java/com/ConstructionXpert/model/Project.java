ee9e1ea7e2d00bd0fe9cdc7b1eb492ff3ff9bcec
oussamasine07 <sineoussama@gmail.com>
2025-03-17 00:14:07 +0000

    add project model

---
+++ src/main/java/com/ConstructionXpert/model/Project.java
@@ -0,0 +1,70 @@
+package com.ConstructionXpert.model;
+
+public class Project {
+
+    private int projectId;
+    private String name;
+    private String description;
+    private String startDate;
+    private String endDate;
+    private double budget;
+    public Admin admin;
+
+    public Project () {}
+
+    public int getProjectId() {
+        return projectId;
+    }
+
+    public void setProjectId(int projectId) {
+        this.projectId = projectId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(String startDate) {
+        this.startDate = startDate;
+    }
+
+    public String getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(String endDate) {
+        this.endDate = endDate;
+    }
+
+    public double getBudget() {
+        return budget;
+    }
+
+    public void setBudget(double budget) {
+        this.budget = budget;
+    }
+
+    public Admin getAdmin() {
+        return admin;
+    }
+
+    public void setAdmin(Admin admin) {
+        this.admin = admin;
+    }
+}
