2669520e548d64b75f22526a2ccdf89b7e3de4a3
oussamasine07 <sineoussama@gmail.com>
2025-03-18 13:40:55 +0000

    start project crud

---
+++ src/main/java/com/ConstructionXpert/controller/project/CreateServlet.java
@@ -0,0 +1,28 @@
+package com.ConstructionXpert.controller.project;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+@WebServlet("/project/create")
+public class CreateServlet extends HttpServlet {
+    public void init () {
+
+    }
+
+    protected void doGet (HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException
+    {
+
+    }
+
+    protected void doPost (HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException
+    {
+
+    }
+}
