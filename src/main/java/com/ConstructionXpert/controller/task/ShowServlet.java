6428f2710cc3cfbc5be6aa024c004557d814a84d
oussamasine07 <sineoussama@gmail.com>
2025-03-21 16:01:30 +0000

    start show task details

--- src/main/java/com/ConstructionXpert/controller/task/ShowServlet.java
+++ src/main/java/com/ConstructionXpert/controller/task/ShowServlet.java
@@ -1,4 +1,30 @@
 package com.ConstructionXpert.controller.task;
 
-public class ShowServlet {
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+@WebServlet("/tasks/show")
+public class ShowServlet extends HttpServlet {
+
+    public void init () {
+
+    }
+
+    protected void doGet (HttpServletRequest req, HttpServletResponse res)
+        throws ServletException, IOException
+    {
+
+        //int projectId
+
+        RequestDispatcher rd = req.getRequestDispatcher("/views/task/show.jsp");
+        rd.forward(req, res);
+
+    }
+
 }
